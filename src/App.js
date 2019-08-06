14546b242b0d0b847130a87592033874e1b0b810
Samil96 <1628112@utp.edu.pe>
2019-08-06 16:24:19 -0500

    Vista uno

--- src/App.js
+++ src/App.js
@@ -4,22 +4,12 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="container">
+   <h1 className="title">
+      Hola Lesly, eres un ángel <span aria-label="emoji" role="img">🔥</span>
+   </h1>
+   <input type="text" className="new-task"/>
+</div>
   );
 }
 
