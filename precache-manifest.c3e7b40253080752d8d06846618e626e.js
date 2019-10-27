102e5850a16dbddaf8fdced22c5896ede765b8b7
Samil96 <1628112@utp.edu.pe>
2019-10-27 16:23:12 -0500

    Updates

---
+++ precache-manifest.c3e7b40253080752d8d06846618e626e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29c648783f80f61c232c9f0e7819e1bd",
+    "url": "/samil96/react-pry.git/index.html"
+  },
+  {
+    "revision": "7addfc5af8eecd5c6d6c",
+    "url": "/samil96/react-pry.git/static/css/main.3dfe3662.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/samil96/react-pry.git/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "7addfc5af8eecd5c6d6c",
+    "url": "/samil96/react-pry.git/static/js/main.6584f7c1.chunk.js"
+  },
+  {
+    "revision": "2c9e33311b2707051014",
+    "url": "/samil96/react-pry.git/static/js/runtime~main.815b6fb2.js"
+  }
+]);
